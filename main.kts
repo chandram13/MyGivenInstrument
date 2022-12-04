17ffa1161c00310b35c45128070c3cfb1d5a93d1
Marvish Subhas Chandra <mschandr@uci.edu>
2022-12-04 08:42:01 -0800

    Add files via upload

---
+++ main.kts
@@ -0,0 +1,18 @@
+// Marvish Chandra
+
+public class instrumentPreferences{
+    fun chosenTypes(personality){
+        if personality == "analytical":
+        println("You are someone who is quiet and intelligent, both characteristics that demonstrate you would like to play the piano.")
+        if personality == "energetic":
+        println("You are someone who is restless and have a lot of energy. You would enjoy playing in percussion. Especially the drums.")
+        if personality == "sociable":
+        println("You are someone who is artistic, potentially sensitive. You would play the trombone and would really like it if you enjoy jazz music.")
+        if personality == "outgoing":
+        println("You would enjoy using the bassoon because of the instrument's responsive nature.")
+        if personality == "extroverted":
+        println("Being someone who has a brash and extroverted nature, you would enjoy using the trumpet. It's an important instrument that has a high melody.")
+        if personality == "quiet":
+        println("You are someone who may be awkward, or have a well behaved nature. You would be fit to play the violin because of your private nature.")
+    }
+}
